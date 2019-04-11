a118d7888239890075575b7663537d3364f6b3ca
EliLauff <eli.lauffenburger@gmail.com>
2019-04-11 17:21:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.round(Math.random() * 100);
+
+const mod = 14 % 10;
+
+const max = Math.max(1, 5, 7, 2, 3, 20);
